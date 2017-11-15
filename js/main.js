dfad8f66d144e8828642649c06c746335f7c76bc
Valentina <valentina.opazo@gmail.com>
2017-11-15 19:22:11 -0300

    tarea-16

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+$('#boton_color').click(function(){
+  $( 'body' ).css('background-color', $('#color_ingles').val());
+});
+
+
+function myFunction() {
+    document.getElementById("color_ingles").style.backgroundColor = "green";
+}
